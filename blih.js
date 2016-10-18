3d1c1d6746eb390234fee1271a07b6d4c95fcc4b
Benoit Sida <benoit.sida@epitech.eu>
2016-10-18 17:33:34 +0200

    blih ok

--- blih.js
+++ blih.js
@@ -6,171 +6,233 @@ const prompt = require('prompt-sync')();
 const utf8 = require('utf8');
 const crypto = require('crypto');
 const request = require('request');
-const stringify = require('json-stable-stringify')
+const stringify = require('json-stable-stringify');
+const fs = require('fs');
 
 class Blih {
-    constructor(options) {
-        this.options = options;
-        this._baseUrl = options.baseurl || 'https://blih.epitech.eu/';
-        this._user = options.user || this.get_user();
-        this._token = options.token || this.token_calc();
-        this._verbose = options.verbose || false;
-        this._userAgent = options.useragent || 'blih-' + program.version;
-    }
+	constructor(options) {
+		this.options = options;
+		this._baseUrl = options.baseurl || 'https://blih.epitech.eu/';
+		this._user = options.user || this.get_user();
+		this._token = options.token || this.token_calc();
+		this._verbose = options.verbose || false;
+		this._userAgent = options.useragent || 'blih-' + program.version;
+	}
 
-    sign_data(data) {
-        const sign = crypto.createHmac('sha512', this._token);
-        return {
-            user: this._user,
-            signature: sign.update(this._user + stringify(data, {space: "    "})).digest('hex'),
-            data: data
-        };
-    }
+	sign_data(data) {
+		const sign = crypto.createHmac('sha512', this._token);
+		sign.update(this._user);
+		if (data)
+			sign.update(stringify(data, {space: "    "}));
+		return {
+			user: this._user,
+			signature: sign.digest('hex'),
+			data: data
+		};
+	}
 
-    request(options) {
-        const signed_data = this.sign_data(options.data);
-        request({
-            url: options.path,
-            baseUrl: this._baseUrl,
-            method: options.method || 'GET',
-            body: JSON.stringify(signed_data),
-            headers: {
-                'Content-Type': options.contentType || 'application/json',
-                'User-Agent': this._userAgent
-            }
-        }, function (err, res, body) {
-            if (err)
-              return console.error('request failed:', err);
-            return console.log(body);
-        });
-    }
+	request(options, cb) {
+		cb = cb || ((res) => { console.log(res.message); });
+		const signed_data = this.sign_data(options.data);
+		request({
+			url: options.path,
+			baseUrl: this._baseUrl,
+			method: options.method || 'GET',
+			body: JSON.stringify(signed_data),
+			headers: {
+				'Content-Type': options.contentType || 'application/json',
+				'User-Agent': this._userAgent
+			}
+		}, function (err, res, body) {
+			body = JSON.parse(body);
+			if (body.error) {
+				console.error(`HTTP Error ${res.statusCode}\nError message: '${body.error}'`);
+				process.exit(0);
+			}
+			cb(body);
+		});
+	}
 
-    get_user() {
-        return this.options.user || process.env.LOGNAME || process.env.USER ||
-            process.env.LNAME || process.env.USERNAME;
-    }
+	get_user() {
+		return this.options.user || process.env.LOGNAME || process.env.USER ||
+			process.env.LNAME || process.env.USERNAME;
+	}
 
-    token_calc() {
-        return crypto.createHash('sha512').update(prompt('Password: ', {echo: '*'})).digest('hex');
-    }
-
-    repo_create(name, type, description) {
-        var data = { name, type: type || 'git' };
-        if (description)
-            data['description'] = description;
-        return this.request({ path: '/repositories', method:'POST', data: data });
-    }
+	token_calc() {
+		return crypto.createHash('sha512').update(prompt(`${this._user}'s password: `, {echo: '*'}) || process.exit(1)).digest('hex');
+	}
 }
 
-class subcommand {
-    constructor(options) {
-        this.options = options;
-    }
+class Subcommand {
+	constructor(options) {
+		this.options = options;
+		this.usage_message = '';
+	}
+
+	usage() {
+		console.log(this.usage_message);
+	}
 }
 
-class Repository extends subcommand {
-    create(params) {
-        if (params.length < 1)
-            return this.usage()
-        const blih = new Blih(this.options)
-        blih.repo_create(params[0]);
-    }
+class Repository extends Subcommand {
+	constructor(options) {
+		super(options);
+		this.usage_message =`
+		Usage: ${process.argv[1]} [options] repository command ...
+		
+		Commands :
+		    create repo\t\t\t-- Create a repository named "repo"
+		    info repo\t\t\t-- Get the repository metadata
+		    getacl repo\t\t\t-- Get the acls set for the repository
+		    list\t\t\t-- List the repositories created
+		    setacl repo user [acl]\t-- Set (or remove) an acl for "user" on "repo"
+		    \t\t\t\tACL format:
+		    \t\t\t\t\tr for read
+		    \t\t\t\t\tw for write
+		    \t\t\t\t\ta for admin`;
+		this.usage_message = this.usage_message.substr(3).replace(/\n\t\t/g, '\n');
+	}
 
-    list() {
-        console.log(process.argv);
-    }
+	create(params) {
+		if (params.length < 1)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: '/repositories', method:'POST', data: {
+			name: params[0],
+			type: 'git',
+			description: ''
+		}});
+	}
 
-    info() {
-        console.log(process.argv);
-    }
+	list() {
+		const blih = new Blih(this.options);
+		blih.request({ path: '/repositories' }, (res) => {
+			if (res.hasOwnProperty("repositories"))
+				for (let repo in res.repositories)
+					console.log(repo);
+		});
+	}
 
-    delete() {
-        console.log(process.argv);
-    }
+	info(params) {
+		if (params.length < 1)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: `/repository/${params[0]}` }, (res) => {
+			console.log('Repository info :');
+			console.log(`name : ${params[0]}`);
+			if (res.hasOwnProperty("message"))
+				for (let info in res.message)
+					console.log(`${info} : ${res.message[info]}`);
+		});
+	}
 
-    setacl() {
-        console.log(process.argv);
-    }
+	delete(params) {
+		if (params.length < 1)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: `/repository/${params[0]}`, method:'DELETE' });
+	}
 
-    getacl() {
-        console.log(process.argv);
-    }
+	setacl(params) {
+		if (params.length < 2)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: `/repository/${params[0]}/acls`, method:'POST', data: {
+			user: params[1],
+			acl: params[2] || ''
+		}});
+	}
 
-    usage() {
-        console.log('Usage: ' + process.argv[1] + ' [options] repository command ...')
-        console.log()
-        console.log('Commands :')
-        console.log('\tcreate repo\t\t\t-- Create a repository named "repo"')
-        console.log('\tinfo repo\t\t\t-- Get the repository metadata')
-        console.log('\tgetacl repo\t\t\t-- Get the acls set for the repository')
-        console.log('\tlist\t\t\t\t-- List the repositories created')
-        console.log('\tsetacl repo user [acl]\t\t-- Set (or remove) an acl for "user" on "repo"')
-        console.log('\t\t\t\t\tACL format:')
-        console.log('\t\t\t\t\tr for read')
-        console.log('\t\t\t\t\tw for write')
-        console.log('\t\t\t\t\ta for admin')
-    }
+	getacl(params) {
+		if (params.length < 1)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: `/repository/${params[0]}/acls` }, (res) => {
+			for (let info in res)
+				console.log(`${info}:${res[info]}`);
+		});
+	}
 }
 
-class sshkey extends subcommand {
-    list() {
+class SSHKey extends Subcommand {
+	constructor(options) {
+		super(options);
+		this.usage_message = `
+		Usage: ${process.argv[1]} [options] sshkey command ...
+		
+		Commands :
+		    upload [file]\t\t\t-- Upload a new ssh-key
+		    list\t\t\t\t-- List the ssh-keys
+		    delete [sshkey]\t\t\t-- Delete the sshkey with comment [sshkey]`;
+		this.usage_message = this.usage_message.substr(3).replace(/\n\t\t/g, '\n');
+	}
 
-    }
+	list() {
+		const blih = new Blih(this.options);
+		blih.request({ path: '/sshkeys' }, (res) => {
+			console.log();
+			for (let info in res)
+				console.log(`${info}:\n${res[info]}\n`);
+		});
+	}
 
-    upload() {
+	upload(params) {
+		if (params.length < 1)
+			return this.usage();
+		const file = params[0];
+		const blih = new Blih(this.options);
+		fs.readFile(file, 'utf8', (err, data) => {
+			if (err) {
+				console.error(`Can't open file : ${file}`);
+				process.exit(1);
+			}
+			blih.request({ path: '/sshkeys', method: 'POST', data: { sshkey: data.trim() } });
+		});
+	}
 
-    }
-
-    delete() {
-
-    }
-
-    usage() {
-
-    }
+	delete(params) {
+		if (params.length < 1)
+			return this.usage();
+		const blih = new Blih(this.options);
+		blih.request({ path: `/sshkey/${params[0]}`, method: 'DELETE' });
+	}
 }
 
+program.version('1.7')
+	   .option('-v, --verbose', 'Enable verbose mode [ actually no difference ]')
+	   .option('-u, --user <user>', 'Set user')
+	   .option('-b, --baseurl <url>', 'Set baseurl')
+	   .option('-t, --token <token>', 'Set token')
+	   .option('-U, --useragent <agent>', 'Set useragent');
 
-program
-.version('1.7')
-.option('-v, --verbose', 'Enable verbose mode')
-.option('-u, --user <user>', 'Set user')
-.option('-b, --baseurl <url>', 'Set baseurl')
-.option('-t, --token <token>', 'Set token')
-.option('-U, --useragent <agent>', 'Set useragent');
+program.command('repository [params...]')
+	   .description('Manages repositories')
+	   .action(function (params) {
+		   var action = params.shift();
+		   const repo = new Repository(program);
+		   if (typeof repo[action] !== 'function')
+			   action = 'usage';
+		   repo[action](params);
+	   });
 
-program
-.command('repository [params...]')
-.description('Manages repositories')
-.action(function (params) {
-    const action = params.shift();
-    var repo = new Repository(program);
-    if (typeof repo[action] === 'function')
-        repo[action](params);
-    else
-        repo.usage();
-});
+program.command('sshkey [params...]')
+	   .description('Manages SSH keys')
+	   .action(function (params) {
+		   var action = params.shift();
+		   const sshkeys = new SSHKey(program);
+		   if (typeof sshkeys[action] !== 'function')
+			   action = 'usage';
+		   sshkeys[action](params);
+	   });
 
-program
-.command('sshkey')
-.description('Manages SSH keys')
-.action(function (params) {
-    const action = params.unshift();
-    var sshkeys= new sshkey(program);
-    if (typeof sshkeys[action] === 'function')
-        sshkeys[action]();
-    else
-        sshkeys.usage();
-});
-
-program
-.command('whoami')
-.description('Ask who you are')
-.action(function() {
-    console.log('whoami');
-});
+program.command('whoami')
+	   .description('Ask who you are')
+	   .action(function() {
+		   const blih = new Blih(program);
+		   blih.request({ path: "/whoami" });
+	   });
 
 program.parse(process.argv);
 
 if (program.args.length === 0)
-  program.help();
+	program.help();
