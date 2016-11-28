bdb65374202ee4bb39f66a76099eaac7b55828f7
Benoit Sida <benoit.sida@epitech.eu>
2016-11-28 10:24:49 +0100

    reset to blih only

--- glih/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-window.addEventListener('load', () => {
-  render(<h2>Welcome to React!</h2>, document.getElementById('App'));
-});
